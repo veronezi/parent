3b8afa2ffab9bef6467992f5dfeece072abe4ac2
Thiago Veronezi <thiago@veronezi.org>
2018-11-07 13:08:02 -0500

    add build number to version

--- .travis.prepare.sh
+++ .travis.prepare.sh
@@ -5,4 +5,4 @@ cp .travis.settings.xml $HOME/.m2/settings.xml
 sed -i 's/my_password/'"$BINTRAY_PASSWORD"'/g' $HOME/.m2/settings.xml
 
 mvn clean -q
-mvn -q versions:set -DnewVersion=$MY_VERSION
+mvn -q versions:set -DnewVersion=$MY_VERSION-b$TRAVIS_BUILD_NUMBER
